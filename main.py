3711c2c569e2a9db9b040e9c7effbb5644a4834c
tetyorkin <tetyorkin@gmail.com>
2020-03-07 15:57:03 +0600

    Рабочий вариант

--- main.py
+++ main.py
@@ -2,28 +2,17 @@ import requests
 import time
 import json
 
-TOKEN = ''
+TOKEN = '73eaea320bdc0d3299faa475c196cfea1c4df9da4c6d291633f9fe8f83c08c4de2a3abf89fbc3ed8a44e1'
 URL = 'https://api.vk.com/method/'
 VERSION_API = '5.103'
 
 # (eshmargunov) и id (171691064)
-main_user = ''
+main_user = '171691064'
 
 
-# Список друзей пользователя
-def get_friends(user_id):
-    method = 'friends.get'
-    url = f'{URL}{method}'
-    params = {
-        'access_token': TOKEN,
-        'user_id': user_id,
-        'v': VERSION_API
-    }
-    response = requests.get(url, params=params)
-    count = response.json()['response']['count']
-    user_list = (response.json()['response'])
-    user_list_in = user_list['items']
-    return user_list_in
+class VkApiError(Exception):
+    def __init__(self, text):
+        self.txt = text
 
 
 # Список сообществ пользователя
@@ -37,14 +26,12 @@ def get_groups(user):
         'count': 1000
     }
     response = requests.get(url, params=params)
-    # print(response.json())
-    count = response.json()['response']['count']
     group_list = (response.json()['response'])
     group_list_in = group_list['items']
     return group_list_in
 
 
-# Список пользователей в группе
+# Список пользователей в группе, с фильтрацией по друзьям, параметр 'filter': 'friends'
 def entry_user(user_id):
     method = 'groups.getMembers'
     url = f'{URL}{method}'
@@ -67,11 +54,13 @@ def entry_user(user_id):
     return ex_group_list
 
 
+# Информация о группах в которой состоит пользователь и создание списка по заданному шаблону
 def group_info(user_id):
     method = 'groups.getById'
     url = f'{URL}{method}'
     group_list = entry_user(user_id)
-    to_json = {}
+    # to_json_dict = {}
+    to_json_list = []
     for group_id in group_list:
         params = {
             'access_token': TOKEN,
@@ -79,26 +68,31 @@ def group_info(user_id):
             'v': VERSION_API,
             'fields': 'members_count'
         }
-        response = requests.get(url, params=params)
-        for dict_group in response.json()['response']:
-            name = dict_group['name']
-            gid = dict_group['id']
-            members_count = dict_group['members_count']
-            to_json_1 = {"name": name, "gid": gid, "members_count": members_count}
-            to_json.update(to_json_1)
-    print(to_json)
+        try:
+            response = requests.get(url, params=params)
+            if 'error' in response.json().keys():
+                time.sleep(0.5)
+                error = response.json()['error']['error_msg']
+                raise VkApiError(f'Ошибка! {error}')
 
+            for dict_group in response.json()['response']:
+                name = dict_group['name']
+                gid = dict_group['id']
+                members_count = dict_group['members_count']
+                to_json_1 = {"name": name, "gid": gid, "members_count": members_count}
+                to_json_list.append(to_json_1)
+        except VkApiError as msg:
+            print(msg)
+
+    return to_json_list
 
 
 def wr_json_file(user_id):
     name_file = f'without friends_id{user_id}.json'
     with open(name_file, 'w', encoding='utf-8') as f:
-        f.write(json.dumps(group_info(user_id), sort_keys=True, indent=4, ensure_ascii=False))
-    print(f'Файл со словарём групп пользователя, где нет его друзей создан! Имя файла {name_file}')
+        f.write(json.dumps(group_info(user_id), indent=4, ensure_ascii=False))
+    print(f"Файл со словарём групп пользователя, где нет его друзей создан! Имя файла '{name_file}'")
 
 
 if __name__ == '__main__':
-    # group_info(main_user)
     wr_json_file(main_user)
-    # print(entry_user(main_user))
-# print(get_friends(user_id))
